0e43ba3e6732d607e5c3bdc6ca699d64feb31dfd
Szilárd Nérei <szilardnerei@gmail.com>
2022-10-20 22:28:53 +0200

    kész

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 // Importáld be a HumanHero és TransformerHero osztályokat.
-
+import {HumanHero} from "./hero";
+import {TransformerHero} from "./transformer";
 
 /**
  * Állítsd be helyesen a változót!
@@ -8,7 +9,12 @@
  * Hozz létre benne 3 HumanHero példányt, szabadon választott adatokkal.
  * @var {HumanHero[]} humans
  */
-export const humans: any[] = [];
+export const humans: HumanHero[] = [
+    {id: 1, name: "A", sex: "male", age: 23, health: 1},
+    {id: 2, name: "B", sex: "male", age: 33, health: 2},
+    {id: 3, name: "C", sex: "female", age: 43, health: 3}
+]
+
 
 /**
  * Állítsd be helyesen a változót!
@@ -17,4 +23,8 @@ export const humans: any[] = [];
  * Hozz létre benne 3 TransformerHero példányt, szabadon választott adatokkal.
  * @var {TransformerHero[]} transformers
  */
-export const transformers: any[] = [];
+export const transformers: TransformerHero[] = [
+    {id: 1, name: "X", wings: 2, wheels: 4, clan: "Clan1"},
+    {id: 2, name: "Y", wings: 2, wheels: 0, clan: "Clan2"},
+    {id: 3, name: "Z", wings: 2, wheels: 4, clan: "Clan3"}
+];
